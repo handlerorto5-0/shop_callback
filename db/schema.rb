16d82e494e6d2f47a48078401dbb43baf92ec7e0
kv-2 <str_token@mail.ru>
2019-06-12 16:56:01 +0300

    Callback Default Values
    after_initialize

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_132217) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "login"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
