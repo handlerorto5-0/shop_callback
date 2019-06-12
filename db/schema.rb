0912d2a1a20f04ab5c8cec5557326297e9a44af1
kv-2 <str_token@mail.ru>
2019-06-12 17:08:51 +0300

    add ,odel Orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_12_140659) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
